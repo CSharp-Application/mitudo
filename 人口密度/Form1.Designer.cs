c62b575d05e924c6705aa121e6e118114435cc8e
CSharp-Application <62175446+CSharp-Application@users.noreply.github.com>
2021-01-16 15:20:14 +0900

    Add project

---
+++ 人口密度/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace 人口密度
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.km2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.hito = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.kekka = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // km2
+            // 
+            this.km2.Location = new System.Drawing.Point(12, 49);
+            this.km2.Name = "km2";
+            this.km2.Size = new System.Drawing.Size(100, 23);
+            this.km2.TabIndex = 0;
+            this.km2.TextChanged += new System.EventHandler(this.change);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 15F);
+            this.label1.Location = new System.Drawing.Point(118, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "㎢";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 15F);
+            this.label2.Location = new System.Drawing.Point(262, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 28);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "人";
+            // 
+            // hito
+            // 
+            this.hito.Location = new System.Drawing.Point(156, 49);
+            this.hito.Name = "hito";
+            this.hito.Size = new System.Drawing.Size(100, 23);
+            this.hito.TabIndex = 2;
+            this.hito.TextChanged += new System.EventHandler(this.change);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic UI", 20F);
+            this.label3.Location = new System.Drawing.Point(91, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 37);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "人口密度";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "結果↓";
+            // 
+            // kekka
+            // 
+            this.kekka.AutoSize = true;
+            this.kekka.Font = new System.Drawing.Font("Yu Gothic UI", 15F);
+            this.kekka.Location = new System.Drawing.Point(9, 112);
+            this.kekka.Name = "kekka";
+            this.kekka.Size = new System.Drawing.Size(113, 28);
+            this.kekka.TabIndex = 6;
+            this.kekka.Text = "1㎢当たり人";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 144);
+            this.Controls.Add(this.kekka);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.hito);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.km2);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Font = new System.Drawing.Font("Yu Gothic UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "人口密度";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox km2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox hito;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label kekka;
+    }
+}
+
---
+++ 人口密度/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 人口密度
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void change(object sender,EventArgs e)
+        {
+            double i_km2 = 0;
+            double i_hito = 0;
+            if(!double.TryParse(km2.Text,out i_km2))
+            {
+                kekka.Text = "1㎢当たり人";
+                return;
+            }
+            if(!double.TryParse(hito.Text,out i_hito))
+            {
+                kekka.Text = "1㎢当たり人";
+                return;
+            }
+            kekka.Text = $"1㎢当たり{i_hito / i_km2}人";
+        }
+    }
+}
